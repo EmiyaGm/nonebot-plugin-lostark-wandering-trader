05c6f5eeee34c353917242501dcca322f87bd541
EmiyaGm <464723943@qq.com>
2024-07-02 14:39:16 +0800

    update: 0.2.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except FileNotFoundError:
 
 setuptools.setup(
     name="nonebot_plugin_lostark_wandering_trader",
-    version="0.2.3",
+    version="0.2.5",
     license='MIT',
     author="EmiyaGm",
     author_email="464723943@qq.com",
